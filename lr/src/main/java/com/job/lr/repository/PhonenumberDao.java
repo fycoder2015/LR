b1826a8cf2d1dc66020a34665e23dc9dea3ef7d2
cnliuy <cnliuy@163.com>
2015-09-22 12:51:12 +0800

    注册接口调整

--- lr/src/main/java/com/job/lr/repository/PhonenumberDao.java
+++ lr/src/main/java/com/job/lr/repository/PhonenumberDao.java
@@ -1,11 +1,13 @@
 
 package com.job.lr.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.job.lr.entity.Phonenumber;
 
 
 public interface PhonenumberDao extends PagingAndSortingRepository<Phonenumber, Long> {
-	Phonenumber findByPhonenumber(String phonenumber);
+	List <Phonenumber> findByPhonenumberOrderByIdDesc(String phonenumber);
 }
